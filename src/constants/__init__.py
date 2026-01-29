9908f9684aa33f8ee1994926a3ada82926634e27
Sidd900 <siddhantpanigrahi75@gmail.com>
2026-01-30 01:40:00 +0530

    completed project

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://siddhantpanigrahi791_db_user:LQ55yLoF78M3D401@cluster0.gew0imy.mongodb.net/?appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
